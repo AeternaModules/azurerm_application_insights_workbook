cabb85feff5f8469b5c952d8f61eb0c063758e4a
Daniel Allen <dan@aeternagroup.net>
2026-07-13 19:30:54 +0100

    Update to schema version v4.80.0

--- variables.tf
+++ variables.tf
@@ -24,9 +24,9 @@ EOT
     location             = string
     name                 = string
     resource_group_name  = string
-    category             = optional(string) # Default: "workbook"
+    category             = optional(string)
     description          = optional(string)
-    source_id            = optional(string) # Default: "azure monitor"
+    source_id            = optional(string)
     storage_container_id = optional(string)
     tags                 = optional(map(string))
     identity = optional(object({
